5f4efb55b1ceaec538cba18f97385cf651871d9a
Manav Patel <64374123+mpat247@users.noreply.github.com>
2024-02-04 00:17:32 -0500

    setting up mongo

---
+++ server/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const port = process.env.PORT || 3001
+
+app.get('/', (req,res) => {
+    res.json({msg: 'Welcome'})
+});
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=> {
+        app.listen(port, () => {
+            console.log('Successfully Connected to Database & Listening on Port', port);
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
+
+
