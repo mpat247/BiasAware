f610c59de0a66d12b1d317b0b2331c8c07372abf
mpat247 <manav1.patel@ryerson.ca>
2024-02-12 21:47:43 -0500

    mongo setup done

---
+++ server/dbConfig.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const uri = "mongodb+srv://manav:biasaware@biasaware.ipjjs0e.mongodb.net/capstone?retryWrites=true&w=majority";
+
+const clientOptions = { 
+    serverApi: { 
+        version: '1', 
+        strict: true, 
+        deprecationErrors: true 
+    } 
+};
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB: ", error);
+    process.exit(1);
+  }
+}
+
+module.exports = connectToDatabase;
