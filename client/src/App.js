158620d9772182a6c2bb9929df7860b38db43e2f
arshroop26 <118218321+arshroop26@users.noreply.github.com>
2024-02-15 18:25:36 -0500

    emotions wip

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home.js';
 import Statistics from './Statistics.js';
 import Activities from './Activities.js';
-
+import Emotions from './Emotions.js';
 
 import './App.css';
 
@@ -14,6 +14,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Statistics" element={<Statistics />} />
         <Route path="/Activities" element={<Activities />} />
+        <Route path="/Emotions" element={<Emotions />} />
+
 
         {/* Define other routes here if needed */}
       </Routes>
---
+++ client/src/Emotions.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Emotions = () => {
+  return (
+    <div className ="emotions-page">
+      <p style={{ 
+            color: 'Black', 
+            
+          }}>
+        Bye Manav!
+      </p>
+    </div>
+  );
+};
+
+export default Emotions;
