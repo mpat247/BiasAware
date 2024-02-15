5e56ed012a3f7ae0845762212141c58726a53ad4
pdhruvii <92284156+pdhruvii@users.noreply.github.com>
2024-02-15 03:08:51 -0500

    Adding images on slider (keeps original dimensions, no distortions)

--- client/src/SimpleSlider.js
+++ client/src/SimpleSlider.js
@@ -3,43 +3,70 @@ import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './SimpleSlider.css'; // Make sure the path is correct
+import img1 from './1.jpg'; // Adjust the path as necessary
+import square from './square.jpg'; // Adjust the path as necessary
 
 const SimpleSlider = () => {
     return (
         <div className="carousel-container">
-            {/* <h1 className="header-title">Activities</h1> */}
             <Carousel infiniteLoop showStatus={false}>
                 {/* Slide 1 */}
                 <div className="box-row">
-                    <div className="box yellow">Box 1</div>
-                    <div className="box orange">Box 2</div>
-                    <div className="box peach">Box 3</div>
+                    <div className="box yellow">
+                        <div className="image-container">
+                            <img src={img1} alt="Pikachu" className="box-image"/>
+                        </div>
+                    </div>
+                    <div className="box orange">
+                        <div className="image-container">
+                            <img src={square} alt="Square" className="box-image"/>
+                        </div>
+                    </div>
+                    <div className="box peach">
+                        <div className="image-container">
+                            <img src="image3.jpg" alt="Image 3" className="box-image" />
+                        </div>
+                    </div>
                 </div>
                 
                 {/* Slide 2 */}
                 <div className="box-row">
-                    {/* Assign different colors to boxes in Slide 2 */}
-                    <div className="box pink">Box 4</div>
-                    <div className="box magenta">Box 5</div>
-                    <div className="box purple">Box 6</div>
+                    <div className="box pink">
+                        <div className="image-container">
+                            <img src="image4.jpg" alt="Image 4" className="box-image" />
+                        </div>
+                    </div>
+                    <div className="box magenta">
+                        <div className="image-container">
+                            <img src="image5.jpg" alt="Image 5" className="box-image" />
+                        </div>
+                    </div>
+                    <div className="box purple">
+                        <div className="image-container">
+                            <img src="image6.jpg" alt="Image 6" className="box-image" />
+                        </div>
+                    </div>
                 </div>
 
                 {/* Slide 3 */}
                 <div className="box-row">
-                    <div className="box yellow">Box 1</div>
-                    <div className="box orange">Box 2</div>
-                    <div className="box peach">Box 3</div>
+                    <div className="box yellow">
+                        <div className="image-container">
+                            <img src="image7.jpg" alt="Image 7" className="box-image" />
+                        </div>
+                    </div>
+                    <div className="box orange">
+                        <div className="image-container">
+                            <img src="image8.jpg" alt="Image 8" className="box-image" />
+                        </div>
+                    </div>
+                    <div className="box peach">
+                        <div className="image-container">
+                            <img src="image9.jpg" alt="Image 9" className="box-image" />
+                        </div>
+                    </div>
                 </div>
-                
-                {/* Slide 4 */}
-                <div className="box-row">
-                    {/* Assign different colors to boxes in Slide 3 */}
-                    <div className="box pink">Box 7</div>
-                    <div className="box magenta">Box 8</div>
-                    <div className="box purple">Box 9</div>
-                </div>
-        </Carousel>
-
+            </Carousel>
         </div>
     );
 };
