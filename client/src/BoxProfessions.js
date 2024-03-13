d1f561d1b0baa0a27acb76c9545eacda9c0f9f0a
pdhruvii <92284156+pdhruvii@users.noreply.github.com>
2024-03-13 19:10:45 -0400

    no horiz overflow, vertical overflow

--- client/src/BoxProfessions.js
+++ client/src/BoxProfessions.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import Slider from 'react-slick';
-// import "slick-carousel/slick/slick.css"; 
-// import "slick-carousel/slick/slick-theme.css";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './BoxProfessions.css';
 
 const BoxProfessions = () => {
-
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4, // Change this to 4
+        slidesToScroll: 1, // Change this to 4
+        vertical: true,
+        verticalSwiping: true
+    };
 
     return (
         <div className="box-container">
             <div className="box-content">
                 <div className="left-box"></div>
                 <div className="middle-box"></div>
-                <div class="right-boxes-container">
-                    <div class="box"></div>
-                    <div class="box"></div>
-                    <div class="box"></div>
-                    <div class="box"></div>
+                <div className="right-boxes-container">
+                    <Slider {...settings}>
+                        <div className="box"></div>
+                        <div className="box"></div>
+                        <div className="box"></div>
+                        <div className="box"></div>
+                    </Slider>
                 </div>
             </div>
         </div>
