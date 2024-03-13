d69245970a66fbaaf621b1315b3197d2be04e536
pdhruvii <92284156+pdhruvii@users.noreply.github.com>
2024-03-13 18:35:10 -0400

    removed slider, 4 boxes within aqua

--- client/src/BoxProfessions.js
+++ client/src/BoxProfessions.js
@@ -1,57 +1,22 @@
 import React from 'react';
 import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
+// import "slick-carousel/slick/slick.css"; 
+// import "slick-carousel/slick/slick-theme.css";
 import './BoxProfessions.css';
 
-function SampleNextArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-        <div
-            className={className}
-            style={{ ...style, display: "block", borderRadius: "50%" }}
-            onClick={onClick}
-        />
-    );
-}
-
-function SamplePrevArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-        <div
-            className={className}
-            style={{ ...style, display: "block", borderRadius: "50%" }}
-            onClick={onClick}
-        />
-    );
-}
-
 const BoxProfessions = () => {
-    const settings = {
-        infinite: true,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        vertical: true,
-        verticalSwiping: true,
-        nextArrow: <SampleNextArrow />,
-        prevArrow: <SamplePrevArrow />,
-    };
+
 
     return (
         <div className="box-container">
             <div className="box-content">
                 <div className="left-box"></div>
                 <div className="middle-box"></div>
-                <div className="right-boxes-container">
-                    {/* <div className="underlay-right-boxes-container"> */}
-                        <Slider {...settings}>
-                            <div className="box"></div>
-                            <div className="box"></div>
-                            <div className="box"></div>
-                            <div className="box"></div>
-                        </Slider>
-                    {/* </div> */}
+                <div class="right-boxes-container">
+                    <div class="box"></div>
+                    <div class="box"></div>
+                    <div class="box"></div>
+                    <div class="box"></div>
                 </div>
             </div>
         </div>
