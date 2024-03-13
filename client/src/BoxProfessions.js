565948f65b676a1bd060d6b71c7dba117b2ecf32
pdhruvii <92284156+pdhruvii@users.noreply.github.com>
2024-03-13 14:04:16 -0400

    Restarted, added box-container and box-content w/o slider

--- client/src/BoxProfessions.js
+++ client/src/BoxProfessions.js
@@ -1,83 +1,15 @@
-import React, { useState } from 'react';
-import Slider from 'react-slick'; // Importing the Slider component
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-import './BoxProfessions.css'; // Ensure this is the correct path to your CSS file
-
-// Import your images - adjust paths as necessary
-import basketball from './images/image 7.png';
-import cricket from './images/image 8.png';
-import volleyball from './images/image 9.png';
-import hockey from './images/image 10.png';
-import bingo from './images/image 11.png';
-import tennis from './images/image 12.png';
+import React from 'react';
+import './BoxProfessions.css'; // Make sure this is the correct path
 
 const BoxProfessions = () => {
-    const [currentSlide, setCurrentSlide] = useState(0);
-
-    const settings = {
-        dots: true,
-        infinite: true,
-        vertical: true,
-        verticalSwiping: true,
-        beforeChange: (current, next) => setCurrentSlide(next),
-        slidesToShow: 4, // Adjust based on your display needs
-        slidesToScroll: 1,
-        adaptiveHeight: true // This will adjust the height based on the slide content
-        // margin: '0',
-        // padding: '0'
-    };
-
     return (
-        <div className="box-professions-container">
-        <Slider {...settings}>
-            <div className="slide-container"> {/* New container for the slide */}
-                <div className="box yellow">
-                    <div className="image-container">
-                        <img src={basketball} alt="basketball" className="box-image"/>
-                    </div>
-                </div>
-                <div className="box orange">
-                    <div className="image-container">
-                        <img src={cricket} alt="cricket" className="box-image"/>
-                    </div>
-                </div>
-                <div className="box peach">
-                    <div className="image-container">
-                        <img src={volleyball} alt="volleyball" className="box-image"/>
-                    </div>
-                </div>
-                <div className="box pink">
-                    <div className="image-container">
-                        <img src={hockey} alt="hockey" className="box-image"/>
-                    </div>
-                </div>
+        <div className="box-container">
+            <div className="box-content">
+                <h2>Professions</h2>
+                <p>This is your main content for the Professions page.</p>
+                {/* You can add more content here as needed */}
             </div>
-            <div className="slide-container"> {/* New container for the slide */}
-                <div className="box yellow">
-                    <div className="image-container">
-                        <img src={basketball} alt="basketball" className="box-image"/>
-                    </div>
-                </div>
-                <div className="box orange">
-                    <div className="image-container">
-                        <img src={cricket} alt="cricket" className="box-image"/>
-                    </div>
-                </div>
-                <div className="box peach">
-                    <div className="image-container">
-                        <img src={volleyball} alt="volleyball" className="box-image"/>
-                    </div>
-                </div>
-                <div className="box pink">
-                    <div className="image-container">
-                        <img src={hockey} alt="hockey" className="box-image"/>
-                    </div>
-                </div>
-            </div>
-            {/* Add more slides as needed */}
-        </Slider>
-    </div>
+        </div>
     );
 };
 
