caf4e975291e7a13ceca8caf2d619889dac4304a
mpat247 <manav1.patel@ryerson.ca>
2024-03-15 11:53:24 -0400

    deploying

--- server/app.js
+++ server/app.js
@@ -15,6 +15,13 @@ const { GridFSBucket } = require('mongodb');
 
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({extended:false}));
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+})
 
 app.get('/', (req, res) => {
   res.json({ msg: 'Welcome to the MERN Capstone Project' });
