8f9e4a76943b2db4ce587c20bb21930a155d58ae
pdhruvii <92284156+pdhruvii@users.noreply.github.com>
2024-03-15 02:09:17 -0400

    hover effect and text

--- client/src/ProfessionsLanding.js
+++ client/src/ProfessionsLanding.js
@@ -10,36 +10,36 @@ import LabourIcon from './images/LabourProf.png'; // Adjust path as necessary
 const ProfessionsLanding = () => {
     return (
         <div className="landing-page">
-            {/* { <button className="icon business" onClick={() => {}}>
-                <img src={BusinessIcon} alt="Business" />
-            </button> */}
-            {/* <button className="icon healthcare" onClick={() => {}}>
-                <img src={HealthcareIcon} alt="Healthcare" />
-            </button> */}
-            {/* <button className="icon labour" onClick={() => {}}>
-                <img src={LabourIcon} alt="Labour" />
-            </button>  */}
-            {/* <button className="icon education" onClick={() => {}}>
-                <img src={EducationIcon} alt="Education" />
-            </button> */}
-            <Link className="icon business" to="/Professions">
-                <img src={BusinessIcon} alt="Business" />
-            </Link>
-            <Link className="icon healthcare" to="/Professions">
-                <img src={HealthcareIcon} alt="Healthcare" />
-            </Link>
-            <Link className="icon maintenance" to="/Professions">
-                <img src={MaintenanceIcon} alt="Maintenance" />
-            </Link>
-            <Link className="icon education" to="/Professions">
-                <img src={EducationIcon} alt="Education" />
-            </Link>
-            <Link className="icon labour" to="/Professions">
-                <img src={LabourIcon} alt="Labour" />
-            </Link>
-            {/* <button className="icon maintenance" onClick={() => {}}>
-                <img src={MaintenanceIcon} alt="Maintenance" />
-            </button> */}
+            <div className="icon business">
+                <Link to="/Professions">
+                    <img src={BusinessIcon} alt="Business" />
+                </Link>
+                <span>BUSINESS</span>
+            </div>
+            <div className="icon healthcare">
+                <Link to="/Professions">
+                    <img src={HealthcareIcon} alt="Healthcare" />
+                </Link>
+                <span>HEALTHCARE</span>
+            </div>
+            <div className="icon maintenance">
+                <Link to="/Professions">
+                    <img src={MaintenanceIcon} alt="Maintenance" />
+                </Link>
+                <span>MAINTENANCE</span>
+            </div>
+            <div className="icon education">
+                <Link to="/Professions">
+                    <img src={EducationIcon} alt="Education" />
+                </Link>
+                <span>EDUCATION</span>
+            </div>
+            <div className="icon labour">
+                <Link to="/Professions">
+                    <img src={LabourIcon} alt="Labour" />
+                </Link>
+                <span>LABOUR</span>
+            </div>
         </div>
     );
 };
