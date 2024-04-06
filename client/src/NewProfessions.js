1df583564defd335bcde9a7d32d4443bd77b7902
pdhruvii <92284156+pdhruvii@users.noreply.github.com>
2024-04-06 16:35:05 -0400

    professions.js before adding sections

--- client/src/NewProfessions.js
+++ client/src/NewProfessions.js
@@ -4,13 +4,7 @@ import './NewProfessions.css';
 
 const NewProfessions = () => {
     return (
-        <div className="spinner">
-            <div className="category slice1"></div>
-            <div className="category slice2"></div>
-            <div className="category slice3"></div>
-            <div className="category slice4"></div>
-            <div className="category slice5"></div>
-        </div>
+        <div className="spinner"></div>
     );
 };
 
