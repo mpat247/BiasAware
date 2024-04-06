f2b79ad981853f5873003eb3e916bc84befaca11
pdhruvii <92284156+pdhruvii@users.noreply.github.com>
2024-04-06 16:17:09 -0400

    small rotating circle, messed up pie

--- client/src/NewProfessions.js
+++ client/src/NewProfessions.js
@@ -1,21 +1,17 @@
-import React, { useEffect, useState } from 'react';
+// NewProfessions.js
+import React from 'react';
 import './NewProfessions.css';
 
 const NewProfessions = () => {
     return (
-        <div>
-            {/* <h1>Professions</h1> */}
-            {/* <BoxProfessions /> */}
-            {/* <BoxProfessions /> */}
-
-            
-
-            
+        <div className="spinner">
+            <div className="category category1"></div>
+            <div className="category category2"></div>
+            <div className="category category3"></div>
+            <div className="category category4"></div>
+            <div className="category category5"></div>
         </div>
     );
 };
 
 export default NewProfessions;
-
-
-
