10662f1cea2fc96cf6465cd0547ff75b28ee2e6f
pdhruvii <92284156+pdhruvii@users.noreply.github.com>
2024-04-06 17:55:51 -0400

    back to orig spinner

--- client/src/NewProfessions.js
+++ client/src/NewProfessions.js
@@ -1,24 +1,11 @@
+// NewProfessions.js
 import React from 'react';
 import './NewProfessions.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell, faComment, faSmile, faHeart, faStar } from '@fortawesome/free-solid-svg-icons';
 
 const NewProfessions = () => {
-  return (
-    <div id="wrapper">
-      <div id="wheel">
-        <div id="inner-wheel">
-          <div className="sec"><FontAwesomeIcon icon={faBell} /></div>
-          <div className="sec"><FontAwesomeIcon icon={faComment} /></div>
-          <div className="sec"><FontAwesomeIcon icon={faSmile} /></div>
-          <div className="sec"><FontAwesomeIcon icon={faHeart} /></div>
-          <div className="sec"><FontAwesomeIcon icon={faStar} /></div>
-        </div>
-        <div id="shine"></div>
-      </div>
-      <div id="txt"></div>
-    </div>
-  );
+    return (
+        <div className="spinner"></div>
+    );
 };
 
 export default NewProfessions;
