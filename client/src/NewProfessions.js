2e3f6e8a33e0acf5e93db84846f0b75ff6462786
pdhruvii <92284156+pdhruvii@users.noreply.github.com>
2024-04-06 18:22:58 -0400

    deconstructed pie

--- client/src/NewProfessions.js
+++ client/src/NewProfessions.js
@@ -1,10 +1,17 @@
-// NewProfessions.js
 import React from 'react';
 import './NewProfessions.css';
 
 const NewProfessions = () => {
     return (
-        <div className="spinner"></div>
+        <div>
+            <svg viewBox="0 0 63.6619772368 63.6619772368">
+                <circle className="pie1" cx="31.8309886184" cy="31.8309886184" r="15.9154943092"/>
+                <circle className="pie2" cx="31.8309886184" cy="31.8309886184" r="15.9154943092"/>
+                <circle className="pie3" cx="31.8309886184" cy="31.8309886184" r="15.9154943092"/>
+                <circle className="pie4" cx="31.8309886184" cy="31.8309886184" r="15.9154943092"/>
+                <circle className="pie5" cx="31.8309886184" cy="31.8309886184" r="15.9154943092"/>
+            </svg>
+        </div>
     );
 };
 
