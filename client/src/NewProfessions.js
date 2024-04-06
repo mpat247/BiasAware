595599c0f0229e395eccf7595a7c118c9616b56b
pdhruvii <92284156+pdhruvii@users.noreply.github.com>
2024-04-06 17:52:27 -0400

    open source code

--- client/src/NewProfessions.js
+++ client/src/NewProfessions.js
@@ -1,32 +1,24 @@
-// NewProfessions.js
 import React from 'react';
 import './NewProfessions.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell, faComment, faSmile, faHeart, faStar } from '@fortawesome/free-solid-svg-icons';
 
 const NewProfessions = () => {
-    const handleClick = (section) => {
-        console.log(`Section ${section} clicked!`);
-        // Add your click handling logic here
-    };
-
-    return (
-        <div className="spinner">
-            <div className="category" style={{ transform: `rotate(${0}deg)` }} onClick={() => handleClick(1)}>
-                <span className="icon icon1">🎓</span>
-            </div>
-            <div className="category" style={{ transform: `rotate(${72}deg)` }} onClick={() => handleClick(2)}>
-                <span className="icon icon2">💼</span>
-            </div>
-            <div className="category" style={{ transform: `rotate(${144}deg)` }} onClick={() => handleClick(3)}>
-                <span className="icon icon3">🏠</span>
-            </div>
-            <div className="category" style={{ transform: `rotate(${216}deg)` }} onClick={() => handleClick(4)}>
-                <span className="icon icon4">✈️</span>
-            </div>
-            <div className="category" style={{ transform: `rotate(${288}deg)` }} onClick={() => handleClick(5)}>
-                <span className="icon icon5">🎨</span>
-            </div>
+  return (
+    <div id="wrapper">
+      <div id="wheel">
+        <div id="inner-wheel">
+          <div className="sec"><FontAwesomeIcon icon={faBell} /></div>
+          <div className="sec"><FontAwesomeIcon icon={faComment} /></div>
+          <div className="sec"><FontAwesomeIcon icon={faSmile} /></div>
+          <div className="sec"><FontAwesomeIcon icon={faHeart} /></div>
+          <div className="sec"><FontAwesomeIcon icon={faStar} /></div>
         </div>
-    );
+        <div id="shine"></div>
+      </div>
+      <div id="txt"></div>
+    </div>
+  );
 };
 
 export default NewProfessions;
