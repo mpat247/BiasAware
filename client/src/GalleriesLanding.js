453a5b81d7a3935657dffd71034632291f7c8726
pdhruvii <92284156+pdhruvii@users.noreply.github.com>
2024-04-10 02:50:57 -0400

    galleries before trying to implement own dataset + unresponsive caption

--- client/src/GalleriesLanding.js
+++ client/src/GalleriesLanding.js
@@ -5,19 +5,22 @@ import Galleries from './Galleries';
 const GalleriesLanding = () => {
   return (
     <div className="galleries-landing-container">
-        <h1 className="galleries-landing-title">GALLERY</h1>
-        <div className="galleries-landing-all-three-container">
-          <div className="galleries-landing-individual-container">
-            <Galleries />
-          </div>
-          <div className="galleries-landing-individual-container">
-            <Galleries />
-          </div>
-          <div className="galleries-landing-individual-container">
-            <Galleries />
-          </div>
-        </div>
-    </div>
+         <h1 className="galleries-landing-title">GALLERY</h1>
+         <div className="galleries-landing-all-three-container">
+           <div className="galleries-landing-individual-container">
+             <h1 className="galleries-landing-individual-titles">Age</h1>
+             <Galleries />
+           </div>
+           <div className="galleries-landing-individual-container">
+             <h1 className="galleries-landing-individual-titles">Gender</h1>
+             <Galleries />
+           </div>
+           <div className="galleries-landing-individual-container">
+             <h1 className="galleries-landing-individual-titles">Race</h1>
+             <Galleries />
+           </div>
+         </div>
+     </div>
   );
 };
 
