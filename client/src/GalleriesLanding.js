dcf5a73aa403ef37becdc8b86139572d27e0f604
pdhruvii <92284156+pdhruvii@users.noreply.github.com>
2024-04-10 01:07:37 -0400

    added 3 galleries

--- client/src/GalleriesLanding.js
+++ client/src/GalleriesLanding.js
@@ -1,14 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './GalleriesLanding.css';
 import Galleries from './Galleries';
 
 const GalleriesLanding = () => {
-
   return (
     <div className="galleries-landing-container">
-        <h1 classname="galleries-landing-title">GALLERY</h1>
+        <h1 className="galleries-landing-title">GALLERY</h1>
         <div className="galleries-landing-all-three-container">
-
+          <div className="galleries-landing-individual-container">
+            <Galleries />
+          </div>
+          <div className="galleries-landing-individual-container">
+            <Galleries />
+          </div>
+          <div className="galleries-landing-individual-container">
+            <Galleries />
+          </div>
         </div>
     </div>
   );
